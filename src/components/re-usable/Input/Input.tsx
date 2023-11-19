ad6c4cdc2f27390fd9d95f8d8c397ac654b76b21
Alp <alpasyon07@gmail.com>
2023-11-19 01:50:31 +0000

    Created the input component

---
+++ src/components/re-usable/Input/Input.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface InputProps {
+	className?: string;
+	type?: string;
+	name?: string;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	value?: string;
+}
+
+const Input: FC<InputProps> = ({ className, type, name, onChange, value }) => {
+	return <input className={'input ' + className} type={type} name={name} onChange={onChange} value={value} />;
+};
+
+export default Input;
