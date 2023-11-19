3005342f52de0cf549bdc78ffff68ca1a6b89230
Alp <alpasyon07@gmail.com>
2023-11-19 00:17:06 +0000

    fixed auth middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,57 @@
 'use client'
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserClient } from '@supabase/ssr'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 import type { Database } from '@/lib/database.types'
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
-  const supabase = createClientComponentClient<Database>()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const router = useRouter()
+	const supabase = createBrowserClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
-  const handleSignUp = async () => {
-	await supabase.auth.signUp({
-	  email,
-	  password,
-	  options: {
-		emailRedirectTo: `${location.origin}/auth/callback`,
-	  },
-	})
-	router.refresh()
-  }
+	const handleSignUp = async () => {
+		await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+			emailRedirectTo: `${location.origin}/auth/callback`,
+			},
+		})
+		router.refresh()
+	}
 
-  const handleSignIn = async () => {
-	await supabase.auth.signInWithPassword({
-	  email,
-	  password,
-	})
-	router.refresh()
-  }
+	const handleSignIn = async () => {
+		await supabase.auth.signInWithPassword({
+			email,
+			password,
+		}).then((res) => {console.log(res)})
 
-  const handleSignOut = async () => {
-	await supabase.auth.signOut()
-	router.refresh()
-  }
+		router.refresh()
+		router.push('/account')
+	}
 
-  return (
-	<div className='flex flex-col'>
-		<h1>Email</h1>
-		<input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-		<h1>Password</h1>
-		<input
-			type="password"
-			name="password"
-			onChange={(e) => setPassword(e.target.value)}
-			value={password}
-		/>
-		<button onClick={handleSignUp}>Sign up</button>
-		<button onClick={handleSignIn}>Sign in</button>
-		<button onClick={handleSignOut}>Sign out</button>
-	</div>
-  )
+	const handleSignOut = async () => {
+		await supabase.auth.signOut()
+		router.refresh()
+	}
+
+	return (
+		<div className='flex flex-col'>
+			<h1>Email</h1>
+			<input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+			<h1>Password</h1>
+			<input
+				type="password"
+				name="password"
+				onChange={(e) => setPassword(e.target.value)}
+				value={password}
+			/>
+			<button onClick={handleSignUp}>Sign up</button>
+			<button onClick={handleSignIn}>Sign in</button>
+			<button onClick={handleSignOut}>Sign out</button>
+		</div>
+	)
 }
