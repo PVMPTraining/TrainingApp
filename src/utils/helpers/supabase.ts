96751bf326216dbeb47d7266b468cad1e9815e35
Alp <alpasyon07@gmail.com>
2023-11-21 02:37:02 +0000

    add as string to mute error

--- src/utils/helpers/supabase.ts
+++ src/utils/helpers/supabase.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
 
 export async function GetUser() {
 	const { data: user } = await supabase.auth.getUser()
@@ -14,6 +14,6 @@ export async function GetUser() {
 
 // Dev only
 export async function GetUserID(id: string) {
-  const { data: user_id } = await supabase.from('users').select('id').eq('id', id).single()
-  return user_id?.id
+	const { data: user_id } = await supabase.from('users').select('id').eq('id', id).single()
+	return user_id?.id
 }
