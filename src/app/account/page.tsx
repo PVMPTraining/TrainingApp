a2872bc850ccf27c4556a5974bd72ab1379ca173
Alp <alpasyon07@gmail.com>
2023-11-21 01:17:25 +0000

    Supabase helper init

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,16 +1,33 @@
 'use client';
 
-import BodyFatCalculator from '@/src/components/body-fat/BodyFatCalculator';
+import { useEffect, useState } from 'react'
+import { GetUser, GetUserID } from '../../utils/helpers/supabase'
+
 import CalorieCalculator from '@/src/components/calorie-calculator/CalorieCalculator';
+import BodyFatCalculator from '@/src/components/body-fat/BodyFatCalculator';
 
 export default function Account() {
-  return (
-    <>
-      <div>
-        Account
-        {/* <BodyFatCalculator /> */}
-        <CalorieCalculator />
-      </div>
-    </>
-  );
+	const [isLoading, setIsLoading] = useState(true)
+	const [userID, setUserID] = useState<any>([])
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const user = await GetUser()
+			const user_id = await GetUserID(user?.id as string)
+			setUserID(user_id)
+			setIsLoading(false)
+		}
+
+		fetchPosts()
+	  }, [])
+
+	return (
+		<>
+			<div>
+				Account
+				<div>{userID}</div>
+				<CalorieCalculator />
+			</div>
+		</>
+	)
 }
---
+++ src/utils/helpers/supabase.ts
@@ -0,0 +1,19 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
+export async function GetUser() {
+	const { data: user } = await supabase.auth.getUser()
+	return user.user
+}
+
+// export async function GetUsersID() {
+// 	const { data: user } = await supabase.auth.getUser()
+// 	return user?.user?.id
+// }
+
+// Dev only
+export async function GetUserID(id: string) {
+  const { data: user_id } = await supabase.from('users').select('id').eq('id', id).single()
+  return user_id?.id
+}
