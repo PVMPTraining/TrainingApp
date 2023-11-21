9965c84dbb7abcb0c3abdabc73ea7650e047afe8
Alp <alpasyon07@gmail.com>
2023-11-21 17:24:10 +0000

    Implemented formik form to login page and created validation schema

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,47 +17,50 @@ import Input from "@/src/components/re-usable/Input/Input";
 //Types
 import { Database } from "../../types/types";
 
-export default function Login() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+import { useFormik } from 'formik';
 
+import { LoginFormValidationSchema} from '@/src/utils/yup/LoginFormValidationSchema';
+
+export default function Login() {
 	const router = useRouter();
 	const supabase = createBrowserClient<Database>(
 	  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
 	  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 	);
 
-	const handleSignIn = async () => {
-		await supabase.auth
-		  .signInWithPassword({
-			email,
-			password,
-		  })
-		  .then((res) => {
-			console.log(res);
-		  });
+	const formik = useFormik({
+		initialValues: {
+			email: '',
+			password: '',
+		},
+		validationSchema: LoginFormValidationSchema,
+		onSubmit: async (values) => {
+		try {
+			await supabase.auth.signInWithPassword({
+				email: values.email,
+				password: values.password,
+			});
 
-		router.refresh();
-		router.push('/account');
-	  };
+			router.refresh();
+			router.push('/account');
+		} catch (error) {
+			console.error('Sign-in error:', error);
+		}
+		},
+	});
 
 	return (
 		<div className="h-screen flex flex-col justify-end gap-4 p-8">
-		<Link className="fixed top-4 left-4" href='/'>Back</Link>
-		<h1>Email</h1>
-		<Input
-		  name="email"
-		  onChange={(e) => setEmail(e.target.value)}
-		  value={email}
-		/>
-		<h1>Password</h1>
-		<Input
-		  type="password"
-		  name="password"
-		  onChange={(e) => setPassword(e.target.value)}
-		  value={password}
-		/>
-		<Button onClick={handleSignIn}>Sign in</Button>
-	  </div>
-	)
+			<Link className="fixed top-4 left-4" href='/'>Back</Link>
+			<form className="flex flex-col gap-2" onSubmit={formik.handleSubmit}>
+				<h1>Email</h1>
+				<Input className="bg-base-200" type="text" name="email" onChange={formik.handleChange} value={formik.values.email} />
+				{formik.touched.email && formik.errors.email && ( <div className="text-red-600">{formik.errors.email}</div> )}
+				<h1>Password</h1>
+				<Input className="bg-base-200" type="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
+				{formik.touched.password && formik.errors.password && ( <div className="text-red-600">{formik.errors.password}</div> )}
+				<Button type="submit">Sign in</Button>
+			</form>
+		</div>
+	);
 }
