4653de84e5c4b0e2852a09917ba9aefb388ac04a
Honey171 <Tahabaloglu171@outlook.com>
2023-11-22 15:50:24 +0300

    Auto stash before merge of "master" and "origin/master"

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,61 +1,63 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-	let response = NextResponse.next({
-		request: {
-		headers: request.headers,
-		},
-	})
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-		{
-		cookies: {
-			get(name: string) {
-			return request.cookies.get(name)?.value
-			},
-			set(name: string, value: string, options: CookieOptions) {
-			request.cookies.set({
-				name,
-				value,
-				...options,
-			})
-			response = NextResponse.next({
-				request: {
-				headers: request.headers,
-				},
-			})
-			response.cookies.set({
-				name,
-				value,
-				...options,
-			})
-			},
-			remove(name: string, options: CookieOptions) {
-			request.cookies.set({
-				name,
-				value: '',
-				...options,
-			})
-			response = NextResponse.next({
-				request: {
-				headers: request.headers,
-				},
-			})
-			response.cookies.set({
-				name,
-				value: '',
-				...options,
-			})
-			},
-		},
-		}
-	)
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          });
+        },
+      },
+    },
+  );
 
-	await supabase.auth.getSession()
-	const user = await supabase.auth.getUser();
+  // await supabase.auth.getSession()
+  //   const user = await supabase.auth
+  //     .getUser()
+  //     .then((data) => console.log(data.data.user?.aud));
 
 	if(!user.data.user) {
 		switch (request.nextUrl.pathname) {
@@ -79,7 +81,7 @@ export async function middleware(request: NextRequest) {
 		}
 	}
 
-	return response
+  return response;
 }
 
 export const config = {
