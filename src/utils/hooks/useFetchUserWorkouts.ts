23f9c9d004e95fd0a02c69f54a95984f8a206206
Alp <alpasyon07@gmail.com>
2023-11-22 23:05:06 +0000

    Update the useFetchUserWorkouts hook

--- src/utils/hooks/useFetchUserWorkouts.ts
+++ src/utils/hooks/useFetchUserWorkouts.ts
@@ -2,24 +2,23 @@
 
 import { useState, useEffect } from 'react';
 
-import { GetUser, GetUserWorkouts } from '../helpers/supabase';
+import { GetUserID, GetUserWorkouts } from '../helpers/supabase';
 
 const useFetchUserWorkouts = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [userWorkouts, setUserWorkouts] = useState<any>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [userWorkouts, setUserWorkouts] = useState<any>([]);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const user = await GetUser();
-      const user_workouts = await GetUserWorkouts(user?.id as string);
-      setUserWorkouts(user_workouts);
-      setIsLoading(false);
-    };
+	useEffect(() => {
+		const fetchPosts = async () => {
+		const user_workouts = await GetUserWorkouts(await GetUserID() as string);
+		setUserWorkouts(user_workouts);
+		setIsLoading(false);
+		};
 
-    fetchPosts();
-  }, []);
+		fetchPosts();
+	}, []);
 
-  return { isLoading, userWorkouts };
+	return { isLoading, userWorkouts };
 };
 
 export default useFetchUserWorkouts;
