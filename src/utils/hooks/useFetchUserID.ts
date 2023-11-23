33ab22e881c4d4ec96d98b3f8bab7b16d5963e5d
Honey171 <Tahabaloglu171@outlook.com>
2023-11-23 03:48:49 +0300

    try catch blocks

--- src/utils/hooks/useFetchUserID.ts
+++ src/utils/hooks/useFetchUserID.ts
@@ -3,22 +3,27 @@
 import { useState, useEffect } from 'react';
 
 import { GetUserID } from '../helpers/supabase';
+import { Log, LogLevel } from '../helpers/debugLog';
 
 const useFetchUserID = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [userID, setUserID] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userID, setUserID] = useState<any>([]);
 
-	useEffect(() => {
-		const fetchPosts = async () => {
-		const user_id = await GetUserID();
-		setUserID(user_id);
-		setIsLoading(false);
-		};
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const user_id = await GetUserID();
+        setUserID(user_id);
+        setIsLoading(false);
+      } catch (error: any) {
+        Log(LogLevel.ERROR, error.message);
+      }
+    };
 
-		fetchPosts();
-	}, []);
+    fetchPosts();
+  }, []);
 
-	return { isLoading, userID };
+  return { isLoading, userID };
 };
 
 export default useFetchUserID;
--- src/utils/hooks/useFetchUserWorkouts.ts
+++ src/utils/hooks/useFetchUserWorkouts.ts
@@ -3,22 +3,31 @@
 import { useState, useEffect } from 'react';
 
 import { GetUserID, GetUserWorkouts } from '../helpers/supabase';
+import { Log, LogLevel } from '../helpers/debugLog';
 
 const useFetchUserWorkouts = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [userWorkouts, setUserWorkouts] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userWorkouts, setUserWorkouts] = useState<any>([]);
 
-	useEffect(() => {
-		const fetchPosts = async () => {
-		const user_workouts = await GetUserWorkouts(await GetUserID() as string);
-		setUserWorkouts(user_workouts);
-		setIsLoading(false);
-		};
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const user_workouts = await GetUserWorkouts(
+          (await GetUserID()) as string,
+        );
 
-		fetchPosts();
-	}, []);
+        setUserWorkouts(user_workouts);
+        setIsLoading(false);
+      } catch (error: any) {
+        Log(LogLevel.ERROR, error.message);
+        setIsLoading(false);
+      }
+    };
 
-	return { isLoading, userWorkouts };
+    fetchPosts();
+  }, []);
+
+  return { isLoading, userWorkouts };
 };
 
 export default useFetchUserWorkouts;
