52e8d77422a15b2ae8714086a0870a698fed4360
Alp <alpasyon07@gmail.com>
2023-11-26 06:39:27 +0000

    Fix selected behaviour

--- src/components/UI/combobox/combobox.tsx
+++ src/components/UI/combobox/combobox.tsx
@@ -8,21 +8,47 @@ interface ComboBoxProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const ComboBox: FC<ComboBoxProps> = ({ options }) => {
 	// Define an initial state for the input value and the selected option
 	const [inputValue, setInputValue] = useState("");
+	const [selectedOption, setSelectedOption] = useState("");
+	const [inputFocused, setInputFocused] = useState(false);
 
 	// Event handler to handle changes in the input field
 	const handleInputChange = (event: { target: { value: any } }) => {
 		const value = event.target.value;
 		setInputValue(value);
+
+		// Set the selected option based on the input value (if it matches an option)
+		if (options.includes(value)) {
+			setSelectedOption(value);
+		} else {
+			setSelectedOption("");
+		}
 	};
 
 	// Event handler to handle option selection from the dropdown
 	const handleOptionSelect = (option: React.SetStateAction<string>) => {
 		setInputValue(option);
+		setSelectedOption(option);
+	};
+
+	const handleInputFocus = () => {
+		setInputFocused(true);
+	};
+
+	const handleInputBlur = () => {
+		setInputFocused(false);
 	};
 
 	return (
 		<div className="dropdown">
-			<Input tabIndex={0} className="focus:rounded-b-none" type="text" value={inputValue} onChange={handleInputChange} />
+			<Input
+				tabIndex={0}
+				className="focus:rounded-b-none"
+				type="text"
+				value={inputValue}
+				onChange={handleInputChange}
+				onFocus={handleInputFocus}
+				onBlur={handleInputBlur}
+			/>
 			<ul tabIndex={0} className="shadow menu dropdown-content bg-base-100 p-2 w-full rounded-b-md">
 				{options
 					.filter((option) => option.includes(inputValue))
@@ -32,6 +58,7 @@ const ComboBox: FC<ComboBoxProps> = ({ options }) => {
 						</li>
 					))}
 			</ul>
+			{selectedOption && <p>You selected: {selectedOption}</p>}
 		</div>
 	);
 };
