272eaba4841d665f1e63838bf33adbe1b614c71a
Honey171 <Tahabaloglu171@outlook.com>
2023-11-26 22:54:39 +0300

    Calculator improved

--- src/app/fitness/page.tsx
+++ src/app/fitness/page.tsx
@@ -3,12 +3,11 @@ import { FC } from "react";
 
 // Next
 import Link from "next/link";
-import { Button } from "@/src/components/re-usable/Button/Button";
 
 const FitnessPage: FC = () => {
 	return (
 		<>
-			<div className="min-h-screen flex flex-col justify-end gap-4 p-8">Welcome to the Fitness section!</div>
+			<div className="min-h-screen flex flex-col justify-end gap-4 p-8">Welcome to the Fitness Section!</div>
 		</>
 	);
 };
