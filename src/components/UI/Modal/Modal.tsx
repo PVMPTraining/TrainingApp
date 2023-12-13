ea7ef1d6bf96cb77acb69a4c4b4969f097f4fe8d
Alp <alpasyon07@gmail.com>
2023-12-13 05:40:56 +0000

    Create a UI component for modals

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { FC, ButtonHTMLAttributes, ReactNode, useState, useEffect } from "react";
+
+interface ModalProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	openModal: boolean;
+	closeModalCallback: (close: boolean) => void;
+	children: ReactNode;
+}
+
+export const Modal: FC<ModalProps> = ({ openModal, closeModalCallback, className, children, ...props }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	useEffect(() => {
+		setIsModalOpen(openModal);
+	}, [openModal]);
+
+	return (
+		<>
+			{openModal && (
+				<dialog id="my_modal_1" className="modal" open>
+					<div className="modal-box">{children}</div>
+					<div className="modal-backdrop">
+						<button
+							onClick={() => {
+								closeModalCallback(false);
+								setIsModalOpen(false);
+							}}
+						>
+							close
+						</button>
+					</div>
+				</dialog>
+			)}
+		</>
+	);
+};
