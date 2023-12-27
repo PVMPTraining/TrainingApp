9b2b5607aa10a88b5a519941f2df0040ef93b411
Honey171 <Honey171@users.noreply.github.com>
2023-12-27 17:51:05 +0000

    Apply formatting changes

--- src/components/foodSearcher/brandFood/BrandFoodCard.tsx
+++ src/components/foodSearcher/brandFood/BrandFoodCard.tsx
@@ -28,18 +28,18 @@ const BrandFoodCard: FC<BrandFoodCardProps> = ({ food }) => {
 					food.product_name_en
 						? food.product_name_en
 						: food.product_name
-						? food.product_name
-						: food.abbreviated_product_name
-						? food.abbreviated_product_name
-						: food.generic_name_en
-						? food.generic_name_en
-						: food.generic_name_de
-						? food.generic_name_de
-						: food.generic_name_fr
-						? food.generic_name_fr
-						: food.generic_name
-						? food.generic_name
-						: "hidden"
+							? food.product_name
+							: food.abbreviated_product_name
+								? food.abbreviated_product_name
+								: food.generic_name_en
+									? food.generic_name_en
+									: food.generic_name_de
+										? food.generic_name_de
+										: food.generic_name_fr
+											? food.generic_name_fr
+											: food.generic_name
+												? food.generic_name
+												: "hidden"
 				}`}
 				onClick={() => selectBrandFoodHandler(food)}
 			>
@@ -69,16 +69,16 @@ const BrandFoodCard: FC<BrandFoodCardProps> = ({ food }) => {
 						{food.product_name_en
 							? food.product_name_en
 							: food.product_name
-							? food.product_name
-							: food.abbreviated_product_name
-							? food.abbreviated_product_name
-							: food.generic_name_en
-							? food.generic_name_en
-							: food.generic_name_de
-							? food.generic_name_de
-							: food.generic_name_fr
-							? food.generic_name_fr
-							: food.generic_name}
+								? food.product_name
+								: food.abbreviated_product_name
+									? food.abbreviated_product_name
+									: food.generic_name_en
+										? food.generic_name_en
+										: food.generic_name_de
+											? food.generic_name_de
+											: food.generic_name_fr
+												? food.generic_name_fr
+												: food.generic_name}
 					</p>
 					<p className="mt-2">
 						Nutrition score:{" "}
@@ -88,12 +88,12 @@ const BrandFoodCard: FC<BrandFoodCardProps> = ({ food }) => {
 									food.nutrition_grades === "a"
 										? "text-green-500"
 										: food.nutrition_grades === "b"
-										? "text-green-300"
-										: food.nutrition_grades === "c"
-										? "text-yellow-400"
-										: food.nutrition_grades === "d"
-										? "text-orange-400"
-										: "text-red-500"
+											? "text-green-300"
+											: food.nutrition_grades === "c"
+												? "text-yellow-400"
+												: food.nutrition_grades === "d"
+													? "text-orange-400"
+													: "text-red-500"
 								} text-xl`}
 							>
 								{food.nutrition_grades.toUpperCase()}
